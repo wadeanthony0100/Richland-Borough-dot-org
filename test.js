b71138bdd6257232bee6b9d70eb17e5dc1da5459
wade <wadeanthony0100@gmail.com>
2016-02-16 13:27:06 -0500

    Enhanced tests

--- test.js
+++ test.js
@@ -1,27 +1,29 @@
-// Count all of the links from the io.js build page
+'use strict';
+
 var jsdom = require('jsdom');
 var fs = require('fs');
 var chalk = require('chalk');
-var failure = false;
 
-jsdom.env(
-	'index.html', function(err, window){
-		Array.from(window.document.getElementsByTagName('a')).map(function(el){
-      return el.href;
-    }).filter(function(el){
-      return el.substring(0,7) === 'file://';
-    }).forEach(function(el){
-      try {
-        fs.statSync(el.substring(7));
-        console.log(chalk.green('[success]'), el);
-      } catch (err) {
-        console.log(chalk.red('[failure]'), el);
-        failure = true;
-      }
-    });
-	}
-);
-
-if (failure){
-  process.exit(1);
+function testLinks(err, window){
+  console.log(chalk.yellow('\n[testing '+window.document.title+']'));
+  Array.from(window.document.getElementsByTagName('a')).map(function(el){
+    return el.href;
+  }).filter(function(el){
+    return el.substring(0,7) === 'file://';
+  }).forEach(function(el){
+    try {
+      fs.statSync(el.substring(7));
+      console.log(chalk.green('[success]'), el);
+    } catch (err) {
+      console.log(chalk.red('[failure]'), el);
+      process.exit(1);
+    }
+  });
 }
+
+fs.readdirSync('.').filter(function(el){
+  return el.substring(el.length-5) === '.html';
+}).forEach(function(el, index, array){
+  jsdom.env(el, testLinks);
+});
+
